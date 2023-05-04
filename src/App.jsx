215189215200b25dab15beadf701ac6ecedd7a3a
NYN1100 <yusufnimatullayev@gmail.com>
2023-05-04 16:48:54 +0500

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { useEffect } from "react";
 import { useTelegram } from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 const App = () => {
   const { onToggleButton, tg } = useTelegram();
   useEffect(() => {
@@ -12,7 +15,10 @@ const App = () => {
   return (
     <div className="App">
       <Header />
-      <button onClick={onToggleButton}>Toggle</button>
+      <Routes>
+        <Route index element={<ProductList />} />
+        <Route path="form" element={<Form />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,74 @@
+import React, { useCallback, useEffect, useState } from "react";
+import "./Form.css";
+import { useTelegram } from "../../hooks/useTelegram";
+const Form = () => {
+  const [country, setCountry] = useState("");
+  const [street, setStreet] = useState("");
+  const [subject, setSubject] = useState("physical");
+  const { tg } = useTelegram();
+
+  const onSendData = useCallback(() => {
+    const data = {
+      country,
+      street,
+      subject,
+    };
+    tg.sendData(JSON.stringify(data));
+  }, []);
+
+  useEffect(() => {
+    Telegram.WebApp.onEvent("mainButtonClicked", onSendData);
+    return () => {
+      Telegram.WebApp.offEvent("mainButtonClicked", onSendData);
+    };
+  }, []);
+
+  useEffect(() => {
+    tg.MainButton.setParams({
+      text: "Ma'lumotlarni yuborish",
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!street || !country) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+    }
+  }, [country, street]);
+
+  const onChangeCountry = (e) => {
+    setCountry(e.target.value);
+  };
+  const onChangeStreet = (e) => {
+    setStreet(e.target.value);
+  };
+  const onChangeSub = (e) => {
+    setSubject(e.target.value);
+  };
+  return (
+    <div className="form">
+      <h3>Malumaotlaringizni kiriting</h3>
+      <input
+        className="input"
+        type="text"
+        placeholder="Davlat"
+        value={country}
+        onChange={onChangeCountry}
+      />
+      <input
+        className="input"
+        type="text"
+        placeholder="Ko'cha"
+        value={street}
+        onChange={onChangeStreet}
+      />
+      <select value={subject} onChange={onChangeSub} className="select">
+        <option value="physical">Jismoniy shaxs</option>
+        <option value="legal">Yuridik shaxs</option>
+      </select>
+    </div>
+  );
+};
+
+export default Form;
