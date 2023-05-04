4e08457bf68ef06943fce1c1a9c112d7e96f55ec
NYN1100 <yusufnimatullayev@gmail.com>
2023-05-04 18:15:50 +0500

    initial

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -4,14 +4,14 @@ import ProductItem from "../ProductItem/ProductItem";
 import { useTelegram } from "../../hooks/useTelegram";
 
 const products = [
-  { id: "1", title: "Jinsi", price: "5000", description: "Ko'k rang" },
-  { id: "2", title: "Kurtka", price: "3000", description: "Oq rang" },
-  { id: "3", title: "Jinsi 2", price: "1000", description: "Yashil rang" },
-  { id: "4", title: "Kurtka 8", price: "6000", description: "Havorang" },
-  { id: "5", title: "Jinsi 3", price: "7800", description: "Qora rang" },
-  { id: "6", title: "Kurtka 7", price: "12000", description: "Ko'k rang" },
-  { id: "7", title: "Jinsi 4", price: "5100", description: "Pushti rang" },
-  { id: "8", title: "Kurtka 5", price: "8000", description: "Qizil rang" },
+  { id: "1", title: "Jinsi", price: 5000, description: "Ko'k rang" },
+  { id: "2", title: "Kurtka", price: 3000, description: "Oq rang" },
+  { id: "3", title: "Jinsi 2", price: 1000, description: "Yashil rang" },
+  { id: "4", title: "Kurtka 8", price: 6000, description: "Havorang" },
+  { id: "5", title: "Jinsi 3", price: 7800, description: "Qora rang" },
+  { id: "6", title: "Kurtka 7", price: 12000, description: "Ko'k rang" },
+  { id: "7", title: "Jinsi 4", price: 5100, description: "Pushti rang" },
+  { id: "8", title: "Kurtka 5", price: 8000, description: "Qizil rang" },
 ];
 
 const getTotalPrice = (items = []) => {
